adc4ceaf5389f5cc7faf5343467204f904d66c6b
Pandora <imcathalat@gmail.com>
2024-03-15 15:29:38 -0300

    quickstart project complete and the configuration for DRF documentation tutorial

---
+++ tutorial/tutorial/quickstart/serializers.py
@@ -0,0 +1,14 @@
+# Data Representation
+
+from django.contrib.auth.models import Group, User
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer): #hyperlinked relations
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
