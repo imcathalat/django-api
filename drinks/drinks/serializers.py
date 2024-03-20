c45b844a106dd7cb944ccd65fd2ddc1f371976b0
Pandora <imcathalat@gmail.com>
2024-03-20 12:47:00 -0300

    some changes

--- drinks/drinks/serializers.py
+++
@@ -1,9 +0,0 @@
-# the process of going python object to Json
-from rest_framework import serializers
-from .models import Drink
-
-# when we trying to return our model through our API
-class DrinkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Drink
-        fields = ['id', 'name', 'description']
